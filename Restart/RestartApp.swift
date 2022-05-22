537ab36662888211bf58eeaa5d0cfe3c9036e204
Andre Rodrigues <andrecomerciale@gmail.com>
2022-05-22 15:29:39 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by André Almeida on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
